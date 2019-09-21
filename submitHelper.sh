d64320b8f06659fc8585add61431061060963d3f
zitao.li <zitao.li@quvideo.com>
2019-09-21 17:02:27 +0800

    自动化代码提交

--- submitHelper.sh
+++ submitHelper.sh
@@ -9,44 +9,32 @@ git push origin master
 git fetch
 result=$(git tag --list)
 
-echo "当前tag list:"$result
+OLD_IFS="$IFS"
+arr=($result)
+IFS="$OLD_IFS"
 
-if [ "$result" = "" ]; then
-    echo "\e[46当前tag为空，默认置为0.0.0"
-    git tag 0.0.0
-    git push -v origin refs/tags/0.0.0
-    sleep 3
-    echo "\e[46自动发版到MDSpecs"
-    ./publishHelper.sh
-else
-    OLD_IFS="$IFS"
-    arr=($result)
-    IFS="$OLD_IFS"
+lastTag=${arr[${#arr[*]}-1]}
 
-    lastTag=${arr[${#arr[*]}-1]}
+OLD_IFS=$IFS
+IFS='.'
+arr1=($lastTag)
+IFS=$OLD_IFS
 
-    OLD_IFS=$IFS
-    IFS='.'
-    arr1=($lastTag)
-    IFS=$OLD_IFS
+lastchar=${arr1[${#arr1[*]}-1]}
 
-    lastchar=${arr1[${#arr1[*]}-1]}
-
-    latestChar=$[$lastchar+1]
-    echo $latestChar
-    latestTag=${arr1[0]}.${arr1[1]}.$latestChar
-    for((k=0;k<100;k++)) do
-        if [[ "${arr[@]}" =~ $latestTag ]];then
-            latestChar=$[$latestChar+1]
-            latestTag=${arr1[0]}.${arr1[1]}.$latestChar
-        fi
-    done;
-    echo "\e[46自动+1升级tag为："$latestVersion
-    git tag $latestVersion
-    git push -v origin refs/tags/$latestVersion
-    echo "\e[46自动发版到MDSpecs"
-       ./publishHelper.sh
-fi
-
+latestChar=$[$lastchar+1]
+echo $latestChar
+latestTag=${arr1[0]}.${arr1[1]}.$latestChar
+for((k=0;k<100;k++)) do
+    if [[ "${arr[@]}" =~ $latestTag ]];then
+        latestChar=$[$latestChar+1]
+        latestTag=${arr1[0]}.${arr1[1]}.$latestChar
+    fi
+done;
+echo "\e[46自动+1升级tag为："$latestVersion
+git tag $latestVersion
+git push -v origin refs/tags/$latestVersion
+echo "\e[46自动发版到MDSpecs"
+ #   ./publishHelper.sh
 
 echo "--------End--------"
