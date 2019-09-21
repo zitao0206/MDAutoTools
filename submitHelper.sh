52c2f5fd6ed5c58c6ce5badb7cb5b50cf800b562
zitao.li <zitao.li@quvideo.com>
2019-09-21 16:10:15 +0800

    自动化代码提交

--- submitHelper.sh
+++ submitHelper.sh
@@ -11,4 +11,34 @@ result=$(git tag --list)
 
 echo "当前tag list:"$result
 
+if [! $result ]; then
+    latestTag= '0.0.0'
+else
+    OLD_IFS="$IFS"
+    arr=($result)
+    IFS="$OLD_IFS"
 
+    lastTag=${arr[${#arr[*]}-1]}
+
+    OLD_IFS=$IFS
+    IFS='.'
+    arr1=($lastTag)
+    IFS=$OLD_IFS
+
+    lastchar=${arr1[${#arr1[*]}-1]}
+
+    latestChar=$[$lastchar+1]
+    echo $latestChar
+    latestTag=${arr1[0]}.${arr1[1]}.$latestChar
+    for((k=0;k<100;k++)) do
+        if [[ "${arr[@]}" =~ $latestTag ]];then
+            latestChar=$[$latestChar+1]
+            latestTag=${arr1[0]}.${arr1[1]}.$latestChar
+        fi
+    done;
+fi
+
+echo "自动升级tag为："$latestTag
+
+
+echo "--------End--------"
